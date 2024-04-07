5f2593e3a340f775f98eb4ff78e5c43738ad298d
Jaya Bhattacharya <jaya.x.bhattacharya@kp.org>
2024-04-07 16:50:06 -0700

    updated main.js

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
-import '@actions/core'
+// import '@actions/core'
 
-// const core = require('@actions/core')
+const core = require('@actions/core')
 // const github = require('@actions/github')
 // const exec = require('@actions/exec')
 
