922e74b98bbe1ae500a466373cfc3e4c7e4f966c
Jaya Bhattacharya <jaya.x.bhattacharya@kp.org>
2024-04-07 16:45:48 -0700

    updated main.js

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,8 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+import '@actions/core'
+
+// const core = require('@actions/core')
+// const github = require('@actions/github')
+// const exec = require('@actions/exec')
 
 function run() {
   core.notice('Hello from my custom JavaScript Action!')
